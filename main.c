b24ed0d9b13846b7ca45387c72772f6a4ffca06a
jna <vonreal20@gmail.com>
2021-01-27 20:04:22 +0900

    status test case

--- main.c
+++ main.c
@@ -4,7 +4,11 @@ int	ft_printf(const char *format, ...);
 
 int	main(void)
 {
-//	ft_printf("HI %%c is %c. and %%s is %s! thank you\n", 'a', "alphabet");
-	ft_printf("%c%s%c\n", 'a', "bcd", 'e');
+	int a;
+
+	a = 1000;
+	ft_printf("%%%c%s%c 143 is [8f] correct? %x %X minus number: %d, plus number %i unsigend int is %u. and address of a :%p.\n", 'a', "bcd", 'e', 143, 143, -12345, 12345, 4294967295, &a);
+
+	printf("address of a : %#x.\n", &a);
 	return (0);
 }
