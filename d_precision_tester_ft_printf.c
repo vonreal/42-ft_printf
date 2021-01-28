80c512996876eb99d827bbedcab7e6eba0006f72
vonrea <vonreal20@gmail.com>
2021-01-28 19:54:08 +0900

    너무 크고 작은 값 제외

--- d_precision_tester_ft_printf.c
+++ d_precision_tester_ft_printf.c
@@ -29,7 +29,7 @@ int main(void)
     ft_printf("3-10: %.100d\n", INT_MIN);
     //ft_printf("3-11: %.2147483647d\n", INT_MIN);
 
-    // <4> number: .(minus number) (error)
+    // <4> number: .(minus number) 
     // ft_printf("4-0: %.-1d\n", 12345);
     // ft_printf("4-1: %.-10d\n", 12345);
     // ft_printf("4-2: %.-100d\n", 12345);
@@ -43,38 +43,38 @@ int main(void)
     // ft_printf("4-10: %.-100d\n", INT_MIN);
     // ft_printf("4-11: %.-2147483647d\n", INT_MIN);
 
-    // // <5> .(*): * = 0
-    // ft_printf("5-0: %.*d\n", 0, 12345);
-    // ft_printf("5-1: %.*d\n", 0, INT_MAX);
-    // ft_printf("5-2: %.*d\n", 0, INT_MIN);
+    // <5> .(*): * = 0
+    ft_printf("5-0: %.*d\n", 0, 12345);
+    //ft_printf("5-1: %.*d\n", 0, INT_MAX);
+    //ft_printf("5-2: %.*d\n", 0, INT_MIN);
 
-    // // <6> .(*): * = plus number
-    // ft_printf("6-0: %*d\n", 1, 12345);
-    // ft_printf("6-1: %*d\n", 10, 12345);
-    // ft_printf("6-2: %*d\n", 100, 12345);
-    // ft_printf("6-3: %*d\n", INT_MAX, 12345);
-    // ft_printf("6-4: %*d\n", 1, INT_MAX);
-    // ft_printf("6-5: %*d\n", 10, INT_MAX);
-    // ft_printf("6-6: %*d\n", 100, INT_MAX);
-    // ft_printf("6-7: %*d\n", INT_MAX, INT_MAX);
-    // ft_printf("6-8: %*d\n", 1, INT_MIN);
-    // ft_printf("6-9: %*d\n", 10, INT_MIN);
-    // ft_printf("6-10: %*d\n", 100, INT_MIN);
-    // ft_printf("6-11: %*d\n", INT_MAX, INT_MIN);
+    // <6> .(*): * = plus number
+    ft_printf("6-0: %*d\n", 1, 12345);
+    ft_printf("6-1: %*d\n", 10, 12345);
+    ft_printf("6-2: %*d\n", 100, 12345);
+    //ft_printf("6-3: %*d\n", INT_MAX, 12345);
+    ft_printf("6-4: %*d\n", 1, INT_MAX);
+    ft_printf("6-5: %*d\n", 10, INT_MAX);
+    ft_printf("6-6: %*d\n", 100, INT_MAX);
+    //ft_printf("6-7: %*d\n", INT_MAX, INT_MAX);
+    ft_printf("6-8: %*d\n", 1, INT_MIN);
+    ft_printf("6-9: %*d\n", 10, INT_MIN);
+    ft_printf("6-10: %*d\n", 100, INT_MIN);
+    //ft_printf("6-11: %*d\n", INT_MAX, INT_MIN);
 
-    // // <7> .(*): * = minus number
-    // ft_printf("8-0: %.*d\n", -1, 12345);
-    // ft_printf("8-1: %.*d\n", -10, 12345);
-    // ft_printf("8-2: %.*d\n", -100, 12345);
-    // ft_printf("8-3: %.*d\n", INT_MIN, 12345);
-    // ft_printf("8-4: %.*d\n", -1, INT_MAX);
-    // ft_printf("8-5: %.*d\n", -10, INT_MAX);
-    // ft_printf("8-6: %.*d\n", -100, INT_MAX);
-    // ft_printf("8-7: %.*d\n", INT_MIN, INT_MAX);
-    // ft_printf("8-8: %.*d\n", -1, INT_MIN);
-    // ft_printf("8-9: %.*d\n", -10, INT_MIN);
-    // ft_printf("8-10: %.*d\n", -100, INT_MIN);
-    // ft_printf("8-11: %.*d\n", INT_MIN, INT_MIN);
-    // ft_printf("\n");
+    // <7> .(*): * = minus number
+    ft_printf("8-0: %.*d\n", -1, 12345);
+    ft_printf("8-1: %.*d\n", -10, 12345);
+    ft_printf("8-2: %.*d\n", -100, 12345);
+    //ft_printf("8-3: %.*d\n", INT_MIN, 12345);
+    ft_printf("8-4: %.*d\n", -1, INT_MAX);
+    ft_printf("8-5: %.*d\n", -10, INT_MAX);
+    ft_printf("8-6: %.*d\n", -100, INT_MAX);
+    //ft_printf("8-7: %.*d\n", INT_MIN, INT_MAX);
+    ft_printf("8-8: %.*d\n", -1, INT_MIN);
+    ft_printf("8-9: %.*d\n", -10, INT_MIN);
+    ft_printf("8-10: %.*d\n", -100, INT_MIN);
+    //ft_printf("8-11: %.*d\n", INT_MIN, INT_MIN);
+    ft_printf("\n");
     return (0);
 }
--- d_precision_tester_printf.c
+++ d_precision_tester_printf.c
@@ -18,17 +18,17 @@ int main(void)
     printf("3-0: %.1d\n", 12345);
     printf("3-1: %.10d\n", 12345);
     printf("3-2: %.100d\n", 12345);
-    printf("3-3: %.2147483647d\n", 12345);
+    //printf("3-3: %.2147483647d\n", 12345);
     printf("3-4: %.1d\n", INT_MAX);
     printf("3-5: %.10d\n", INT_MAX);
     printf("3-6: %.100d\n", INT_MAX);
-    printf("3-7: %.2147483647d\n", INT_MAX);
+    //printf("3-7: %.2147483647d\n", INT_MAX);
     printf("3-8: %.1d\n", INT_MIN);
     printf("3-9: %.10d\n", INT_MIN);
     printf("3-10: %.100d\n", INT_MIN);
-    printf("3-11: %.2147483647d\n", INT_MIN);
+    //printf("3-11: %.2147483647d\n", INT_MIN);
 
-    // <4> number: .(minus number) (error)
+    // <4> number: .(minus number) 
     // printf("4-0: %.-1d\n", 12345);
     // printf("4-1: %.-10d\n", 12345);
     // printf("4-2: %.-100d\n", 12345);
@@ -44,36 +44,36 @@ int main(void)
 
     // <5> .(*): * = 0
     printf("5-0: %.*d\n", 0, 12345);
-    printf("5-1: %.*d\n", 0, INT_MAX);
-    printf("5-2: %.*d\n", 0, INT_MIN);
+    //printf("5-1: %.*d\n", 0, INT_MAX);
+    //printf("5-2: %.*d\n", 0, INT_MIN);
 
     // <6> .(*): * = plus number
     printf("6-0: %*d\n", 1, 12345);
     printf("6-1: %*d\n", 10, 12345);
     printf("6-2: %*d\n", 100, 12345);
-    printf("6-3: %*d\n", INT_MAX, 12345);
+    //printf("6-3: %*d\n", INT_MAX, 12345);
     printf("6-4: %*d\n", 1, INT_MAX);
     printf("6-5: %*d\n", 10, INT_MAX);
     printf("6-6: %*d\n", 100, INT_MAX);
-    printf("6-7: %*d\n", INT_MAX, INT_MAX);
+    //printf("6-7: %*d\n", INT_MAX, INT_MAX);
     printf("6-8: %*d\n", 1, INT_MIN);
     printf("6-9: %*d\n", 10, INT_MIN);
     printf("6-10: %*d\n", 100, INT_MIN);
-    printf("6-11: %*d\n", INT_MAX, INT_MIN);
+    //printf("6-11: %*d\n", INT_MAX, INT_MIN);
 
     // <7> .(*): * = minus number
     printf("8-0: %.*d\n", -1, 12345);
     printf("8-1: %.*d\n", -10, 12345);
     printf("8-2: %.*d\n", -100, 12345);
-    printf("8-3: %.*d\n", INT_MIN, 12345);
+    //printf("8-3: %.*d\n", INT_MIN, 12345);
     printf("8-4: %.*d\n", -1, INT_MAX);
     printf("8-5: %.*d\n", -10, INT_MAX);
     printf("8-6: %.*d\n", -100, INT_MAX);
-    printf("8-7: %.*d\n", INT_MIN, INT_MAX);
+    //printf("8-7: %.*d\n", INT_MIN, INT_MAX);
     printf("8-8: %.*d\n", -1, INT_MIN);
     printf("8-9: %.*d\n", -10, INT_MIN);
     printf("8-10: %.*d\n", -100, INT_MIN);
-    printf("8-11: %.*d\n", INT_MIN, INT_MIN);
+    //printf("8-11: %.*d\n", INT_MIN, INT_MIN);
     printf("\n");
     return (0);
 }
