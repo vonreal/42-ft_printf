54b0e2a3f8f20aafa2f0e331000f0288d490500a
vonrea <vonreal20@gmail.com>
2021-01-28 14:59:37 +0900

    while오류

--- ft_printf.c
+++ ft_printf.c
@@ -117,8 +117,8 @@ void	print_conversion(char conversion, va_list *ap, int precision)
 		if (precision >= 0)
 		{
 			temp = num;
-			u_num = 1;
-			while (temp > 9)
+			u_num = 0;
+			while (temp > 10)
 			{
 				temp /= 10;
 				u_num++;
