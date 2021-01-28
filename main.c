e41d447a2dd8bfbc9204d5aaead542a6e567458b
jna <vonreal20@gmail.com>
2021-01-28 15:55:46 +0900

    main

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 int	ft_printf(const char *format, ...);
 
@@ -7,8 +8,12 @@ int	main(void)
 	int a;
 
 	a = 1000;
-	ft_printf("%%%c%s%c 143 is [8f] correct? %x %X minus number: %d, plus number %i unsigend int is %u. and address of a :%p.\n", 'a', "bcd", 'e', 143, 143, -12345, 12345, 4294967295, &a);
+	//ft_printf("%%%c%s%c 143 is [8f] correct? %x %X minus number: %d, plus number %i unsigend int is %u. and address of a :%p.\n", 'a', "bcd", 'e', 143, 143, -12345, 12345, 4294967295, &a);
 
-	printf("address of a : %#x.\n", &a);
+	//printf("address of a : %#x.\n", &a);
+	//	ft_printf("%.*s\n", 10, "123");
+	//printf("%.*s\n", 10, "123");
+	ft_printf("%.12X\n", 161616);
+	printf("%.12X\n", 161616);
 	return (0);
 }
